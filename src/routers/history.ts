f38be26e230cc9e3b2a2507d960eab2b88d88dcb
立斌 <wanglibin@juzifenqi.com>
2021-02-21 21:49:49 +0800

    update history

--- src/routers/history.ts
+++ src/routers/history.ts
@@ -1,16 +1,13 @@
-import {
-  createBrowserHistory,
-  BrowserHistoryBuildOptions,
-  createHashHistory,
-  HashHistoryBuildOptions
-} from 'history';
+import { createBrowserHistory, createHashHistory } from 'history';
 const {
   NODE_ENV = 'production',
   REACT_APP_DEPLOY_PATH = '/',
   REACT_APP_ROUTER_MODE = 'history'
 } = process.env;
-
-const historyConfig: BrowserHistoryBuildOptions | HashHistoryBuildOptions = {
+export interface HistoryConfigType {
+  basename: string;
+}
+const historyConfig: HistoryConfigType = {
   basename: NODE_ENV === 'production' ? REACT_APP_DEPLOY_PATH : '/'
 };
 const history =
